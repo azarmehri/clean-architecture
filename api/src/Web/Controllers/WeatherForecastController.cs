a2e7e3b3702b9572c01e66307693a07556981274
a.azarmehri <azarmehri@outlook.com>
2022-03-27 10:10:44 +0430

    Add web files

--- api/src/Web/Controllers/WeatherForecastController.cs
+++ api/src/Web/Controllers/WeatherForecastController.cs
@@ -1,32 +1,14 @@
+﻿using CleanArchitecture.Application.WeatherForecasts.Queries.GetWeatherForecasts;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.Web.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController : ApiControllerBase
 {
-    private static readonly string[] Summaries =
+    [HttpGet]
+    public async Task<IEnumerable<WeatherForecast>> Get()
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return await Mediator.Send(new GetWeatherForecastsQuery());
     }
 }
--- api/src/Web/Program.cs
+++ api/src/Web/Program.cs
@@ -1,8 +1,27 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Infrastructure;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Web.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<ApplicationDbContext>();
 
 builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,10 +35,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHealthChecks("api/health");
 app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSpaStaticFiles();
+}
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+
+app.UseSpa(spa => spa.Options.SourcePath = "ClientApp");
 
 app.Run();
